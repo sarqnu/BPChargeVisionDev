cf1745a4c7c14124701cdb02057e8a5d05e86005
SwamyKNT <118817774+SwamykntBp@users.noreply.github.com>
2023-11-16 15:08:45 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ import { devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-  testDir: './tests',
+  testDir: `..\tests\loginPageValidation.spec.ts`,
   // Maximum time expect() should wait for the condition to be met.
   timeout: 10 * 60 * 1000,
   expect: {
